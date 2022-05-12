05aa40c22f187d2af8b6d2af369849249b21bd13
Idooru <shere1765@gmail.com>
2022-05-12 22:17:43 +0900

    0.5.1.2

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { UserService } from './user.service';
+import { Request, Response } from 'express';
 
 @Controller('user')
 class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get()
+  @Get('getuser')
   getUser(): string {
     return this.userService.getUser();
+  }
+
+  @Get(':id')
+  findOne(@Req() req: Request, @Res() res: Response): Promise<any> {
+    return this.userService.findOne(req, res);
   }
 }
 
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+interface JSON {
+  message: string;
+  params: string;
+}
 
 @Injectable()
 class UserService {
+  async findOne(@Req() req: Request, @Res() res: Response): Promise<string[]> {
+    const { id } = req.params;
+    const responseResult: JSON = {
+      message: 'Hello',
+      params: id,
+    };
+    res.status(201).json(responseResult);
+    return ['hello'];
+  }
+
   getUser(): string {
     return 'Hello User';
   }
